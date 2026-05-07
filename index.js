393d89b252a708f97ae555354bb700a5ed1f791b
Rasheduzzaman <rashedzaman1978@gmail.com>
2026-05-07 22:09:33 +0600

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -36,9 +36,29 @@ async function run() {
 
     // GET all users
     app.get('/users', async (req, res) => {
-      const result = await userCollection.find().toArray();
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
       res.send(result);
     });
+
+    app.get('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id)
+      }
+      const user = await userCollection.findOne(query)
+      console.log('User id', id);
+      res.send(user);
+    })
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id)
+      }
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // Ping MongoDB
     await client.db('admin').command({ ping: 1 });
